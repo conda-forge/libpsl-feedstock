12b1937271e4c6352ae548462b62cbc27648568c
Tom Schoonjans <Tom.Schoonjans@rfi.ac.uk>
2021-09-07 11:14:05 +0100

    Fix macOS ARM64 build

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,10 +1,21 @@
-# Get an updated config.sub and config.guess
-cp $BUILD_PREFIX/share/gnuconfig/config.* ./build-aux
 set -ex
 
+if [[ $(uname) == Darwin ]]; then
+  export PKG_CONFIG=$BUILD_PREFIX/bin/pkg-config
+fi
+
+if [ -z "$MESON_ARGS" ]; then
+  # for some reason this is not set on Linux
+  MESON_ARGS="--buildtype=release --prefix=${PREFIX} --libdir=lib"
+fi
+
+if [[ "$target_platform" == "osx-arm64" && "$CONDA_BUILD_CROSS_COMPILATION" == "1" ]]; then
+  unset _CONDA_PYTHON_SYSCONFIGDATA_NAME
+fi
+
 meson setup builddir \
-	--prefix=$PREFIX \
-	--libdir=lib \
+	 ${MESON_ARGS} \
+        --default-library=shared\
 	-Druntime=libicu \
 	-Dbuiltin=libicu
 ninja -v -C builddir -j ${CPU_COUNT}
